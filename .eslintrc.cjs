50020f1046be6d5c3ece1071dfaa1ab08c9c4186
Hoai An <ngohoaian01031@gmail.com>
2024-03-22 13:28:22 +0700

    trailingComma config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -15,12 +15,12 @@ module.exports = {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
     'react/prop-types': ['off', {}],
     'no-unused-vars': [
       'warn',
-      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
-    ],
-  },
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
+    ]
+  }
 }
